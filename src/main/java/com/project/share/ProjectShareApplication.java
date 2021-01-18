f539e970fab0f07cad522f8a1ae6219ee9673994
whoareyouwhoami <young.woo@hotmail.com>
2021-01-18 22:33:03 +0900

    initial commit

---
+++ src/main/java/com/project/share/ProjectShareApplication.java
@@ -0,0 +1,13 @@
+package com.project.share;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectShareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectShareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/share/ProjectShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.share;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectShareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
