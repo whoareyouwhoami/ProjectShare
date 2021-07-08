11300c69349a1ec12bef2f90ba5fd4c12426094e
whoareyouwhoami <young.woo@hotmail.com>
2021-07-08 10:31:06 +0900

    ignore

--- src/main/java/com/project/share/model/Project.java
+++ src/main/java/com/project/share/model/Project.java
@@ -1,6 +1,5 @@
 package com.project.share.model;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
